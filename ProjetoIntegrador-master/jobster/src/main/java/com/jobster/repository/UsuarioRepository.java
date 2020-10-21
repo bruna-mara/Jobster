db768785e07d434ac9fd474abfba4064e3b12b91
unknown <b.maraa0805@gmail.com>
2020-10-20 21:16:10 -0300

    Atualização Security / Swagger Config / JAR

---
+++ ProjetoIntegrador-master/jobster/src/main/java/com/jobster/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.jobster.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jobster.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public List<Usuario> findByNomeContainingIgnoreCase(String nome);
+	Optional<Usuario> findByEmail(String email);
+
+
+}
---
+++ ProjetoIntegrador-master/jobster/src/main/java/com/jobster/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.jobster.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jobster.model.Usuario;
+import com.jobster.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		Optional<Usuario> user = userRepository.findByEmail(userName);
+		user.orElseThrow(()-> new UsernameNotFoundException(userName +"Not found"));
+		return user.map(UserDetailsImpl::new).get();
+	}
+	
+	
+
+}
